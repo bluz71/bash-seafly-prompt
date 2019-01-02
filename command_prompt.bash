7e6e868df5ab252a4b7442c2bec5a5241ed658ac
Dennis B <bluz71@users.noreply.github.com>
2019-01-02 15:36:23 +1100

    Use Bash [[ ]] instead of Posix [ ] for tests
    There are a number of nice benefits:
    
    - support for pattern matching '=~' (already in use)
    - conflating multiple conditionals into one, e.g [[ foo && bar ]] instead of
      [ foo ] && [ bar ]
    - no need to quote variables to handle variables with spaces
    
    See:
    
    http://mywiki.wooledge.org/BashFAQ/031
    https://serverfault.com/questions/52034/what-is-the-difference-between-double-and-single-square-brackets-in-bash

--- command_prompt.bash
+++ command_prompt.bash
@@ -10,101 +10,101 @@ if [[ "$-" =~ i ]]; then
 fi
 
 _color_terminal=0
-if [ "$TERM" = "xterm-256color" ] || [ "$TERM" = "screen-256color" ]; then
+if [[ $TERM = "xterm-256color" || $TERM = "screen-256color" ]]; then
     _color_terminal=1
 fi
 
 # Colors used in the prompt.
-if [ $_interactive_terminal = 1 ] && [ $_color_terminal = 1 ]; then
-    if [ -z "$SEAFLY_NORMAL_COLOR" ]; then
-        SEAFLY_NORMAL_COLOR="$(tput setaf 111)"
+if [[ $_interactive_terminal = 1 && $_color_terminal = 1 ]]; then
+    if [[ -z $SEAFLY_NORMAL_COLOR ]]; then
+        SEAFLY_NORMAL_COLOR=$(tput setaf 111)
     fi
-    if [ -z "$SEAFLY_ALERT_COLOR" ]; then
-        SEAFLY_ALERT_COLOR="$(tput setaf 203)"
+    if [[ -z $SEAFLY_ALERT_COLOR ]]; then
+        SEAFLY_ALERT_COLOR=$(tput setaf 203)
     fi
-    if [ -z "$SEAFLY_HOST_COLOR" ]; then
-        SEAFLY_HOST_COLOR="$(tput setaf 255)"
+    if [[ -z $SEAFLY_HOST_COLOR ]]; then
+        SEAFLY_HOST_COLOR=$(tput setaf 255)
     fi
-    if [ -z "$SEAFLY_GIT_COLOR" ]; then
-        SEAFLY_GIT_COLOR="$(tput setaf 147)"
+    if [[ -z $SEAFLY_GIT_COLOR ]]; then
+        SEAFLY_GIT_COLOR=$(tput setaf 147)
     fi
-    if [ -z "$SEAFLY_PATH_COLOR" ]; then
-        SEAFLY_PATH_COLOR="$(tput setaf 150)"
+    if [[ -z $SEAFLY_PATH_COLOR ]]; then
+        SEAFLY_PATH_COLOR=$(tput setaf 150)
     fi
-    NOCOLOR="$(tput sgr0)"
+    NOCOLOR=$(tput sgr0)
 fi
 
 # Shorten directory paths to a maximum of four components unless PROMPT_DIRTRIM
 # has already been set.
-if [ -z "$PROMPT_DIRTRIM" ]; then
+if [[ -z $PROMPT_DIRTRIM ]]; then
     PROMPT_DIRTRIM=4
 fi
 
 # Symbols used in the prompt.
-if [ -z "$SEAFLY_PROMPT_SYMBOL" ]; then
+if [[ -z $SEAFLY_PROMPT_SYMBOL ]]; then
     SEAFLY_PROMPT_SYMBOL="❯"
 fi
-if [ -z "$SEAFLY_GIT_PREFIX" ]; then
+if [[ -z $SEAFLY_GIT_PREFIX ]]; then
     SEAFLY_GIT_PREFIX=""
 fi
-if [ -z "$SEAFLY_GIT_SUFFIX" ]; then
+if [[ -z $SEAFLY_GIT_SUFFIX ]]; then
     SEAFLY_GIT_SUFFIX=""
 fi
-if [ -z "$SEAFLY_GIT_DIRTY" ]; then
+if [[ -z $SEAFLY_GIT_DIRTY ]]; then
     SEAFLY_GIT_DIRTY="✗"
 fi
-if [ -z "$SEAFLY_GIT_STAGED" ]; then
+if [[ -z $SEAFLY_GIT_STAGED ]]; then
     SEAFLY_GIT_STAGED="✓"
 fi
-if [ -z "$SEAFLY_GIT_STASH" ]; then
+if [[ -z $SEAFLY_GIT_STASH ]]; then
     SEAFLY_GIT_STASH="⚑"
 fi
-if [ -z "$SEAFLY_GIT_AHEAD" ]; then
+if [[ -z $SEAFLY_GIT_AHEAD ]]; then
     SEAFLY_GIT_AHEAD="↑"
 fi
-if [ -z "$SEAFLY_GIT_BEHIND" ]; then
+if [[ -z $SEAFLY_GIT_BEHIND ]]; then
     SEAFLY_GIT_BEHIND="↓"
 fi
-if [ -z "$SEAFLY_GIT_DIVERGED" ]; then
+if [[ -z $SEAFLY_GIT_DIVERGED ]]; then
     SEAFLY_GIT_DIVERGED="↕"
 fi
 
 _command_prompt()
 {
     # Run a pre-command if set.
-    if [ -n "$SEAFLY_PRE_COMMAND" ]; then
+    if [[ -n $SEAFLY_PRE_COMMAND ]]; then
         eval $SEAFLY_PRE_COMMAND
     fi
 
     # Set a simple prompt for non-interactive or non-color terminals.
-    if [ $_interactive_terminal = 0 ] || [ $_color_terminal = 0 ]; then
+    if [[ $_interactive_terminal = 0 || $_color_terminal = 0 ]]; then
         PS1='\h \w > '
         return
     fi
 
     local git_details=""
-    if [[ "$(git rev-parse --is-inside-work-tree --is-bare-repository 2>/dev/null)" =~ true ]]; then
+    if [[ $(git rev-parse --is-inside-work-tree --is-bare-repository 2>/dev/null) =~ true ]]; then
         local branch="$(git rev-parse --abbrev-ref HEAD 2>/dev/null)"
-        if [ "$branch" = "HEAD" ]; then
+        if [[ $branch = "HEAD" ]]; then
             branch="detached*$(git rev-parse --short HEAD 2>/dev/null)"
         fi
 
         local dirty=""
         local staged=""
-        if [ "$branch" != "detached*" ] &&
-           [ "$GIT_PS1_SHOWDIRTYSTATE" != 0 ] &&
-           [ "$(git config --bool bash.showDirtyState)" != "false" ]; then
+        if [[ $branch != "detached*" &&
+              $GIT_PS1_SHOWDIRTYSTATE != 0 &&
+              $(git config --bool bash.showDirtyState) != "false" ]]; then
             git diff --no-ext-diff --quiet --exit-code --ignore-submodules 2>/dev/null || dirty=$SEAFLY_GIT_DIRTY
             git diff --no-ext-diff --quiet --cached --exit-code --ignore-submodules 2>/dev/null || staged=$SEAFLY_GIT_STAGED
         fi
 
         local stash=""
-        if [ "$GIT_PS1_SHOWSTASHSTATE" != 0 ]; then
+        if [[ $GIT_PS1_SHOWSTASHSTATE != 0 ]]; then
             git rev-parse --verify --quiet refs/stash >/dev/null && stash=$SEAFLY_GIT_STASH
         fi
 
         local upstream=""
-        if [ "$GIT_PS1_SHOWUPSTREAM" != 0 ]; then
+        if [[ $GIT_PS1_SHOWUPSTREAM != 0 ]]; then
             case "$(git rev-list --left-right --count HEAD...@'{u}' 2>/dev/null)" in
             "") # no upstream
                 upstream="" ;;
@@ -120,7 +120,7 @@ _command_prompt()
         fi
 
         local spacer=""
-        if [ -n "$dirty" ] || [ -n "$staged" ] || [ -n "$stash" ] || [ -n "$upstream" ]; then
+        if [[ -n $dirty || -n $staged || -n $stash || -n $upstream ]]; then
             spacer=" "
         fi
         git_details=" $SEAFLY_GIT_PREFIX$branch$spacer\[$SEAFLY_ALERT_COLOR\]$dirty\[$SEAFLY_NORMAL_COLOR\]$staged$upstream\[$SEAFLY_GIT_COLOR\]$stash$SEAFLY_GIT_SUFFIX"
@@ -128,7 +128,7 @@ _command_prompt()
 
     # Normal prompt indicates that the last command ran successfully.
     # Alert prompt indicates that the last command failed.
-    local prompt_end="\$(if [ \$? = 0 ]; then echo \[\$SEAFLY_NORMAL_COLOR\]; else echo \[\$SEAFLY_ALERT_COLOR\]; fi) $SEAFLY_PROMPT_SYMBOL\[\$NOCOLOR\] "
+    local prompt_end="\$(if [[ \$? = 0 ]]; then echo \[\$SEAFLY_NORMAL_COLOR\]; else echo \[\$SEAFLY_ALERT_COLOR\]; fi) $SEAFLY_PROMPT_SYMBOL\[\$NOCOLOR\] "
 
     PS1="\[$SEAFLY_HOST_COLOR\]\h\[$SEAFLY_GIT_COLOR\]$git_details\[$SEAFLY_PATH_COLOR\] \w$prompt_end"
     PS2="\[$SEAFLY_NORMAL_COLOR\]$SEAFLY_PROMPT_SYMBOL\[\$NOCOLOR\] "
