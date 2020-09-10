194b8688eaf6aeb04949753594bcd60e0a9dca6a
Craig Anderson <craig.anderson0@gmail.com>
2020-09-10 10:15:28 +0100

    fixes based on comments on PR

--- command_prompt.bash
+++ command_prompt.bash
@@ -7,8 +7,8 @@
 # Non-interactive shells don't have a prompt, exit early.
 [[ $- =~ i ]] || return 0
 
-# Set a simple prompt for non-256color and non-kitty terminals.
-if [[ $TERM != *-256color ]] && [[ $TERM != *-kitty ]]; then
+# Set a simple prompt for non-256color and non-kitty or alacritty terminals.
+if [[ $TERM != *-256color ]] && [[ $TERM != *-kitty ]] && [[ $TERM != alacritty ]]; then
     PS1='\h \w > '
     return 0
 fi
@@ -180,9 +180,15 @@ _seafly_command_prompt() {
         prompt_prefix="\[$SEAFLY_PREFIX_COLOR\]$prefix_value "
     fi
 
+    if [[ $SEAFLY_MULTILINE = 1 ]]; then
+        prompt_prefix="\n$prompt_prefix"
+    fi
+
     local prompt_start
-    if [[ $SEAFLY_SHOW_USER = 1 ]]; then
+    if [[ $SEAFLY_SHOW_USER = 1 && $SEAFLY_SHOW_HOST = 1 ]]; then
         prompt_start="\[$SEAFLY_HOST_COLOR\]\u@\h"
+    elif [[ $SEAFLY_SHOW_USER = 1 ]]; then
+        prompt_start="\[$SEAFLY_HOST_COLOR\]\u"
     elif [[ $SEAFLY_SHOW_HOST = 1 ]]; then
         prompt_start="\[$SEAFLY_HOST_COLOR\]\h"
     fi
@@ -203,12 +209,10 @@ _seafly_command_prompt() {
     _seafly_colors=("$SEAFLY_ALERT_COLOR" "$SEAFLY_NORMAL_COLOR")
     local prompt_end
     if [[ $SEAFLY_MULTILINE = 1 ]]; then
-        prompt_start="\n$prompt_start"
         prompt_end="\[\n\${_seafly_colors[\$((!\$?))]}\] $SEAFLY_PROMPT_SYMBOL\[\$NOCOLOR\] "
     else
         prompt_end="\[\${_seafly_colors[\$((!\$?))]}\] $SEAFLY_PROMPT_SYMBOL\[\$NOCOLOR\] "
     fi
-    unset _seafly_git
 
     PS1="$prompt_prefix$prompt_start$prompt_middle$prompt_end"
     PS2="\[$SEAFLY_NORMAL_COLOR\]$SEAFLY_PS2_PROMPT_SYMBOL\[\$NOCOLOR\] "
